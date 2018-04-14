790c9e930f42657d63c72624caee99b7ed984346
Callum Gare <callum@gare.com.au>
2018-04-14 15:49:01 +1000

    Adds a few new typography changes
    - The letters in few random words will progressively get smaller
    - Word spacing is ocationaly shrunk
    - A lot styled to look like Alot (and vice versa)

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var leafElms = []
 
 // Made a few changes word levels
 // A few references to House of Leaves
-var house_re = /\w*House\w*|Haus|Maison/i
-var minotaur_re = /Minotaur/i
-var rn_re = /rn/
 var queue = [document.body]
 var node
 
@@ -12,6 +9,7 @@ while (node = queue.pop()) {
     for (var i = 0; i < node.childNodes.length; i++) {
 		var subNode = node.childNodes[i];
 		if (subNode.nodeType === Node.ELEMENT_NODE) {
+			if (subNode.tagName === 'SCRIPT' || subNode.tagName === 'STYLE') continue;
 			queue.push(subNode);
 			if (subNode.childNodes.length === 1 && subNode.childNodes[0].nodeType === Node.TEXT_NODE) {
 				leafElms.push(subNode)
@@ -19,25 +17,37 @@ while (node = queue.pop()) {
 
         } else if (subNode.nodeType === Node.TEXT_NODE) {
 			var match
-			if (match = subNode.textContent.match(house_re)) {
-				splitMatch(match).classList.add('nmt_house')
-
-			} else if (match = subNode.textContent.match(minotaur_re)) {
-				splitMatch(match).classList.add('nmt_minotaur')
-
-			} else if (match = subNode.textContent.match(rn_re)) {
-				subNode.textContent = subNode.textContent.replace(rn_re, 'm')
-			}
-			function splitMatch(match) {
-				var matchNode = subNode.splitText(match.index) 
-				var followingNode = matchNode.splitText(match[0].length)
-				var containerElement = document.createElement('span');
-				containerElement.classList.add('nmt_house')
-				node.removeChild(matchNode)
-				containerElement.appendChild(matchNode);
-				node.insertBefore(containerElement, followingNode);
+			if (match = subNode.textContent.match(/\w*House\w*|Haus|Maison/i)) {
+				splitMatch(match, subNode).classList.add('nmt_house')
 				i += 2
-				return containerElement
+			}
+			if (match = subNode.textContent.match(/Minotaur/i)) {
+				splitMatch(match, subNode).classList.add('nmt_minotaur')
+				i += 2
+			}
+			if (match = subNode.textContent.match(/r(?=n)/)) {
+				splitMatch(match, subNode).classList.add('nmt_tracking-small')
+				i += 2
+			}
+			if (match = subNode.textContent.match(/a(?= lot)/i)) {
+				splitMatch(match, subNode).classList.add('nmt_tracking-very-small')
+				i += 2
+			}
+			if (match = subNode.textContent.match(/a(?=lot)/i)) {
+				splitMatch(match, subNode).classList.add('nmt_tracking-very-big')
+				i += 2
+			}
+			// pick a random word with 7 or more letters and make each letter in that word progressively smaller
+			// applies every 1 in 10 times
+			match = subNode.textContent.match(/\w{7,}/g) 
+			if(match && randomInt(1,10) === 10){
+				match = match[randomInt(0, match.length-1)]
+				//console.log(match)
+				match = subNode.textContent.match(match)
+				var elm = splitMatch(match, subNode)
+				i += 2
+				elm.classList.add('nmt_smaller')
+				nestLetters(elm)
 			}
         }
 	}
@@ -82,4 +92,51 @@ for (var i = 0; i < leafElms.length; i += 1 + Math.ceil(leafElms.length / (100 /
 	var node = leafElms[i]
 	node.classList.add('nmt_tracking')
 	//console.log(node)
+}
+
+Array.from(document.querySelectorAll('p'))
+.filter(elm => randomInt(1, 5) === 1)
+.forEach(elm => {
+	elm.classList.add('nmt_wordspacing')
+	//console.log(elm)
+})
+
+
+/*******************
+ * Util functions
+ *******************/
+// Nicked from here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function randomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+// takes a regex match and a text node and splits out matching text it into a new elm node 
+function splitMatch(match, node) {
+	var matchNode = node.splitText(match.index) 
+	var followingNode = matchNode.splitText(match[0].length)
+	var containerElement = document.createElement('span');
+	node.parentNode.removeChild(matchNode)
+	containerElement.appendChild(matchNode);
+	node.parentNode.insertBefore(containerElement, followingNode);
+	return containerElement
+}
+
+// for an element recursively nest each letter in new elements that have the same classes as the parent element
+// eg <span class="tree">cake</span> would become:
+// <span class="tree">c<span class="tree">a<span class="tree">k<span class="tree">e</span></span></span></span>
+function nestLetters(elm) {
+	// ignore if elm doesn't contain just one node which is a text node
+	if (elm.childNodes.length !== 1 || elm.childNodes[0].nodeType !== Node.TEXT_NODE) return;
+	// base case
+	if(elm.textContent.length <= 1) return;
+	var newChildNode = elm.childNodes[0].splitText(1)
+	var containerElement = document.createElement('span');
+	containerElement.className = elm.className;
+	elm.removeChild(newChildNode)
+	containerElement.appendChild(newChildNode);
+	elm.appendChild(containerElement);
+	nestLetters(containerElement);
+	return containerElement
 }
